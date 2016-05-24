87026104272e23f516b87627a40e20529c0b4711
Phillip T <donutdeflector@tuta.io>
2016-05-23 22:09:52 -0400

    ADDED: malwaredomainslist

--- adblocker/adblocker.sh
+++ adblocker/adblocker.sh
@@ -12,9 +12,13 @@ cd /tmp/adblocker/
 # wget filters
 wget https://easylist.github.io/easylist/easylist.txt
 wget https://easylist.github.io/easylist/easyprivacy.txt
+wget https://www.malwaredomainlist.com/hostslist/hosts.txt
+
+# change all 127.0.0.1s to 0.0.0.0
+sed -i 's/127.0.0.1/0.0.0.0/g' hosts.txt
 
 # merge into single file
-cat easylist.txt easyprivacy.txt >> hosts
+cat easylist.txt easyprivacy.txt hosts.txt >> hosts
 
 # replace host file
 cp /tmp/adblocker/hosts /etc/hosts
