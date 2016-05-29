ab824f07cc2609d35d7851c9a25c6c2e80a1ca79
Phillip T <donutdeflector@tuta.io>
2016-05-29 18:31:01 -0400

    ADDED: Blocked Reddit Tracking Domains

--- adblocker/adblocker.sh
+++ adblocker/adblocker.sh
@@ -20,6 +20,10 @@ sed -i 's/127.0.0.1/0.0.0.0/g' malwaredomainslist.txt
 # merge into single file
 cat easylist.txt easyprivacy.txt malwaredomainslist.txt >> hosts
 
+# block reddit tracking
+echo "0.0.0.0  events.redditmedia.com" >> hosts
+echo "0.0.0.0  out.reddit.com" >> hosts
+
 # replace host file
 cp /tmp/adblocker/hosts /etc/hosts
 
