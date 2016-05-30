d495940544958b4947d422ddbb5ea2f97337b677
Phillip T <donutdeflector@tuta.io>
2016-05-29 22:25:46 -0400

    CHANGED: Uses StevenBlack's filters

--- adblocker/adblocker.sh
+++ adblocker/adblocker.sh
@@ -10,15 +10,7 @@ mkdir /tmp/adblocker/
 cd /tmp/adblocker/
 
 # wget filters
-wget https://easylist.github.io/easylist/easylist.txt
-wget https://easylist.github.io/easylist/easyprivacy.txt
-wget https://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomainslist.txt
-
-# change all 127.0.0.1s to 0.0.0.0
-sed -i 's/127.0.0.1/0.0.0.0/g' malwaredomainslist.txt
-
-# merge into single file
-cat easylist.txt easyprivacy.txt malwaredomainslist.txt >> hosts
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
 # block reddit tracking
 echo "0.0.0.0  events.redditmedia.com" >> hosts
